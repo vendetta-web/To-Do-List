a45a177f2cc1be0aa12ce46bbb4c800c69f72628
Saurabh Dixit <saurabh.dixit@gyansys.com>
2023-08-30 15:26:35 +0530

    tawk add

--- js/main.js
+++ js/main.js
@@ -117,6 +117,16 @@ searchtextbox.addEventListener("input", function(){
 
 
 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64ef118ea91e863a5c10a188/1h92tp5k9';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 
 
 
